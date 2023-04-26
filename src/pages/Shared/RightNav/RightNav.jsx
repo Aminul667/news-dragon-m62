cdbd2bc9847dae19d49a9c3181b8790eec251b5b
Aminul667 <rahathssn@gmail.com>
2023-04-26 12:28:16 +0600

    62-6 Implement Right Side Component and react ions

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import bg from '../../../assets/bg.png';
+import QZone from "../QZone/QZone";
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right Navigation</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Login With</h2>
+      <Button variant="outline-primary" className="mb-2">
+        {" "}
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        {" "}
+        <FaGithub /> Login with GitHub
+      </Button>
+      <div>
+        <h2>Find us on</h2>
+        <ListGroup>
+          <ListGroup.Item><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+          <ListGroup.Item><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+          <ListGroup.Item><FaInstagram></FaInstagram> Instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div>
+        <img src={bg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default RightNav;
