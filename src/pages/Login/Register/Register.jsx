e3d33e4e40d7cf6f8d0c979b8d26a49cbb3eb7c2
Aminul667 <rahathssn@gmail.com>
2023-04-30 12:20:24 +0600

    64-2 Recap Registration and Login

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -15,6 +15,12 @@ const Register = () => {
     const password = form.password.value;
 
     console.log(name, photo, email, password);
+    createUser(email, password)
+    .then(result => {
+      const createdUser = result.user;
+      console.log(createdUser);
+    })
+    .catch(error => console.error(error))
   }
   
   return (
